28dbec28b93f8a57facc126ebe18fe6a21169214
Erikas Neverdauskas <e.neverdauskas@adform.com>
2018-03-11 14:46:15 +0200

    added real words from Silveris vocabulary

--- src/Silverio.Žodynas/Repositories/WordsRepository.cs
+++ src/Silverio.Žodynas/Repositories/WordsRepository.cs
@@ -10,16 +10,17 @@ namespace Silverio.Žodynas.Repositories
         {
             return new[]
             {
-                new WordPair {Id = 1, LithuanianWord = "Šuo", EnglishWord = "Dog"},
-                new WordPair {Id = 2, LithuanianWord = "Katė", EnglishWord = "Cat"},
-                new WordPair {Id = 3, LithuanianWord = "Namas", EnglishWord = "House"},
-                new WordPair {Id = 4, LithuanianWord = "Kalnas", EnglishWord = "Mountain"},
-                new WordPair {Id = 5, LithuanianWord = "Spalva", EnglishWord = "Color"},
-                new WordPair {Id = 6, LithuanianWord = "Apelsinas", EnglishWord = "Orange"},
-                new WordPair {Id = 7, LithuanianWord = "Beždžionė", EnglishWord = "Monkey"},
-                new WordPair {Id = 8, LithuanianWord = "Jūra", EnglishWord = "Sea"},
-                new WordPair {Id = 9, LithuanianWord = "Upė", EnglishWord = "River"},
-                new WordPair {Id = 10, LithuanianWord = "Lėktuvas", EnglishWord = "Airplane"},
+                new WordPair {Id = 1, LithuanianWord = "Pirmadienis", EnglishWord = "Monday"},
+                new WordPair {Id = 2, LithuanianWord = "Šiandien", EnglishWord = "Today"},
+                new WordPair {Id = 3, LithuanianWord = "Metai", EnglishWord = "Year"},
+                new WordPair {Id = 4, LithuanianWord = "Laikrodis", EnglishWord = "Clock"},
+                new WordPair {Id = 5, LithuanianWord = "Valanda", EnglishWord = "Hour"},
+                new WordPair {Id = 6, LithuanianWord = "Pavasaris", EnglishWord = "Spring"},
+                new WordPair {Id = 7, LithuanianWord = "Viso gero", EnglishWord = "Good bye"},
+                new WordPair {Id = 8, LithuanianWord = "Senelis", EnglishWord = "Grandfather, Grandpa, Grandad"},
+                new WordPair {Id = 9, LithuanianWord = "Grybas", EnglishWord = "Mushroom"},
+                new WordPair {Id = 10, LithuanianWord = "Book bag", EnglishWord = "Kuprinė"},
+                new WordPair {Id = 11, LithuanianWord = "Man patinka mėlynas dangus", EnglishWord = "I like blue sky"},
             };
         }
 
@@ -27,9 +28,10 @@ namespace Silverio.Žodynas.Repositories
         {
             return new[]
             {
-                new WordPair {Id = 1, LithuanianWord = "Medis", EnglishWord = "Tree"},
-                new WordPair {Id = 2, LithuanianWord = "Žiema", EnglishWord = "Winter"},
-                new WordPair {Id = 3, LithuanianWord = "Bulvė", EnglishWord = "Potato"},
+                new WordPair {Id = 1, LithuanianWord = "Stogas", EnglishWord = "Roof"},
+                new WordPair {Id = 2, LithuanianWord = "Šiukšlės", EnglishWord = "Recycle"},
+                new WordPair {Id = 3, LithuanianWord = "Parduotuvė", EnglishWord = "Shop, Store"},
+                new WordPair {Id = 4, LithuanianWord = "Aitvaras", EnglishWord = "Kite"},
             };
         }
 
