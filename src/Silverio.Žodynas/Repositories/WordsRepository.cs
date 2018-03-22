5af5a38de363ea85a45659a63ad3f6ce61cc7d1a
Erikas Neverdauskas <e.neverdauskas@adform.com>
2018-03-22 19:07:47 +0200

    added more words to unknown words

--- src/Silverio.Žodynas/Repositories/WordsRepository.cs
+++ src/Silverio.Žodynas/Repositories/WordsRepository.cs
@@ -42,7 +42,12 @@ namespace Silverio.Žodynas.Repositories
                 new WordPair {Id = 8, LithuanianWord = "Senelis", EnglishWord = "Grandfather, Grandpa, Grandad"},
                 new WordPair {Id = 9, LithuanianWord = "Grybas", EnglishWord = "Mushroom"},
                 new WordPair {Id = 10, LithuanianWord = "Kuprinė", EnglishWord = "Book bag"},
-                new WordPair {Id = 11, LithuanianWord = "Man patinka mėlynas dangus", EnglishWord = "I like blue sky"},
+                new WordPair {Id = 11, LithuanianWord = "Žalia", EnglishWord = "Green"},
+                new WordPair {Id = 12, LithuanianWord = "Dramblys", EnglishWord = "Elephant"},
+                new WordPair {Id = 13, LithuanianWord = "Laiptai", EnglishWord = "Stairs"},
+                new WordPair {Id = 14, LithuanianWord = "Sapnas, Svajonė", EnglishWord = "Dream"},
+                new WordPair {Id = 15, LithuanianWord = "Apie ką tu kalbi?", EnglishWord = "What you talking about?"},
+                new WordPair {Id = 16, LithuanianWord = "Man patinka mėlynas dangus", EnglishWord = "I like blue sky"},
             };
         }
     }
